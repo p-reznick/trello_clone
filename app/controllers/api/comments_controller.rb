0067b0155a62aefdbce71b32e1d02915ea090c7f
Preeti Viswanathan <vis.preeti@gmail.com>
2017-10-17 15:41:16 -0700

    add POST api/comments

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,22 @@
+class Api::CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render :create, status: :created
+    else
+      @error = @comment.errors.full_messages.join(', ')
+      render 'api/shared/error', status: :unprocessable_entity
+    end
+  rescue ActionController::ParameterMissing
+    @error = "Invalid comment data provided"
+    render 'api/shared/error', status: :unprocessable_entity
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text, :card_id)
+  end
+
+end
