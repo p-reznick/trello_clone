bcbd10e63055b10086a94957239944570845afb8
Peter Reznick <preznick@gmail.com>
2017-10-19 18:55:50 -0400

    add nav functionality to boards button in topNap

--- app/javascript/components/board_view/CardView.jsx
+++ app/javascript/components/board_view/CardView.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class CardView extends React.Component {
   render() {
     return (
-      <li> -- {this.props.title}</li>
+      <li>
+        -- <Link to="">{this.props.title}</Link>
+      </li>
     )
   }
 }
